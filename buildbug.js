041b298948cff5c9d84b96de5b03a3e195fdf33c
Brian Neisler <neisler.brian@gmail.com>
2014-03-20 13:37:29 -0700

    Remove buildbug task that no longer exists.

--- buildbug.js
+++ buildbug.js
@@ -143,11 +143,6 @@ buildTarget('local').buildFlow(
                         });
                     }
                 }),
-                targetTask("s3EnsureBucket", {
-                    properties: {
-                        bucket: buildProject.getProperty("local-bucket")
-                    }
-                }),
                 targetTask("s3PutFile", {
                     init: function(task, buildProject, properties) {
                         var packedNodePackage = nodejs.findPackedNodePackage(buildProject.getProperty("splitbug.packageJson.name"),
@@ -218,11 +213,6 @@ buildTarget('prod').buildFlow(
                         });
                     }
                 }),
-                targetTask("s3EnsureBucket", {
-                    properties: {
-                        bucket: "airbug"
-                    }
-                }),
                 targetTask("s3PutFile", {
                     init: function(task, buildProject, properties) {
                         var packedNodePackage = nodejs.findPackedNodePackage(buildProject.getProperty("splitbug.packageJson.name"),
@@ -235,7 +225,7 @@ buildTarget('prod').buildFlow(
                         });
                     },
                     properties: {
-                        bucket: "airbug"
+                        bucket: "{{prod-deploy-bucket}}"
                     }
                 })
             ])
